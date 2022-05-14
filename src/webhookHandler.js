0c6fdd173f2f97d04d9fdc35c354153933c47ea9
swh00tw <a6140000@gmail.com>
2022-05-14 18:37:18 +0800

    log id

--- src/webhookHandler.js
+++ src/webhookHandler.js
@@ -1,31 +1,41 @@
 // webhookHandler
 
 const webhookHandler = (event, client) => {
-  if (event.type !== "message" || event.message.type !== "text") {
+  if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
 
   /*
     implement response logic here,
     currently, just echo back the message
-  */
+     */
+  if (event?.source?.hasOwnProperty('groupId')) {
+    // in group chat
+    console.log('source groupId: ', event.source.groupId);
+  } else if (event?.source?.hasOwnProperty('roomId')) {
+    // in nulti-person chat
+    console.log('source roomId: ', event.source.roomId);
+  } else {
+    // in single person chat
+    console.log('source userId: ', event.source.userId);
+  }
 
   return client.replyMessage(event.replyToken, [
     {
-      type: "text",
+      type: 'text',
       text: event.message.text,
     },
     {
-      type: "text",
-      text: "stfu",
+      type: 'text',
+      text: 'stfu',
     },
     {
-      type: "text",
-      text: "你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹",
+      type: 'text',
+      text: '你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹你從桃園新竹',
     },
     {
-      type: "text",
-      text: "太神啦",
+      type: 'text',
+      text: '太神啦',
     },
   ]);
 };
