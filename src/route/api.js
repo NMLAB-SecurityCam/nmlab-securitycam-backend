a9ff8242956f730a2a7fb0ef260d7c5106f7800e
swh00tw <a6140000@gmail.com>
2022-06-04 00:27:01 +0800

    refactor code

---
+++ src/route/api.js
@@ -0,0 +1,52 @@
+import dotenv from 'dotenv-defaults';
+import express from 'express';
+import cors from 'cors';
+import Users from '../Users';
+import { client } from '../client';
+dotenv.config();
+
+// router initialization
+const router = express.Router();
+// middlewares
+router.use(cors());
+router.use(express.json());
+router.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// for our camera services
+// payload {"id": "LineID", img_url: "https://..."}
+router.post('/alert', async (req, res) => {
+  const userObj = await Users.findById(req.body.id);
+  if (userObj?.userId) {
+    const alertMsg = {
+      type: 'text',
+      text: '$ Alert detected! $',
+      emojis: [
+        {
+          index: 0,
+          productId: '5ac21cc5031a6752fb806d5c',
+          emojiId: '003',
+        },
+        {
+          index: 18,
+          productId: '5ac21cc5031a6752fb806d5c',
+          emojiId: '004',
+        },
+      ],
+    };
+    await client.pushMessage(userObj.userId, alertMsg);
+    await client.pushMessage(userObj.userId, {
+      type: 'image',
+      originalContentUrl: req.body.img_url,
+      previewImageUrl: req.body.img_url,
+    });
+    res.status(200).send({ message: 'Message forwarded successfully!' });
+  } else {
+    res.status(400).send({ message: 'UserID not found. Either the user does not exist or user does not register and activate this feature.' });
+  }
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import express from 'express';
-import https from 'https';
-import cors from 'cors';
 import * as line from '@line/bot-sdk';
 import webhookHandler from './webhookHandler';
-import Users from './Users';
 import dotenv from 'dotenv-defaults';
 import mongoose from 'mongoose';
+import { client, LineSDKConfig } from './client';
+import custom_api_router from './route/api';
 dotenv.config();
 
 mongoose
@@ -18,23 +17,8 @@ mongoose
   .then(res => console.log('MongoDB connection created'))
   .catch(err => console.log(`MongoDB connection failed`));
 
-const LineSDKConfig = {
-  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET,
-};
-const client = new line.Client(LineSDKConfig);
-
 const app = express();
-if (process.env.MODE === 'prod') {
-  app.use(line.middleware(LineSDKConfig));
-}
-app.use(cors());
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use('/api', custom_api_router);
 
 // for health check
 app.get('/', (req, res) => {
@@ -42,46 +26,13 @@ app.get('/', (req, res) => {
 });
 
 // for line webhook (response to events)
-app.post('/webhook', (req, res) => {
+app.post('/webhook', line.middleware(LineSDKConfig), (req, res) => {
   console.log('req: ', req);
   Promise.all(
     req.body.events.map(e => {
       webhookHandler(e, client);
     })
   ).then(result => res.json(result));
-});
-
-// for our camera services
-// payload {"id": "LineID", img_url: "https://..."}
-app.post('/alert', async (req, res) => {
-  const userObj = await Users.findById(req.body.id);
-  if (userObj?.userId) {
-    const alertMsg = {
-      type: 'text',
-      text: '$ Alert detected! $',
-      emojis: [
-        {
-          index: 0,
-          productId: '5ac21cc5031a6752fb806d5c',
-          emojiId: '003',
-        },
-        {
-          index: 18,
-          productId: '5ac21cc5031a6752fb806d5c',
-          emojiId: '004',
-        },
-      ],
-    };
-    await client.pushMessage(userObj.userId, alertMsg);
-    await client.pushMessage(userObj.userId, {
-      type: 'image',
-      originalContentUrl: req.body.img_url,
-      previewImageUrl: req.body.img_url,
-    });
-    res.status(200).send({ message: 'Message forwarded successfully!' });
-  } else {
-    res.status(400).send({ message: 'UserID not found. Either the user does not exist or user does not register and activate this feature.' });
-  }
 });
 
 app.listen(process.env.PORT || 5000, () => {
