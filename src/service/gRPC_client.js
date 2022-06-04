1e38a6da5891c6e156959c6eff81e6c780ff28bd
Gting6 <b07901090@ntu.edu.tw>
2022-06-04 16:25:05 +0800

    node to gRPC success

--- src/service/gRPC_client.js
+++ src/service/gRPC_client.js
@@ -1,9 +1,27 @@
 import * as grpc from 'grpc';
+// import protoLoader from "@grpc/proto-loader"
 const PROTO_PATH = __dirname + '/video.proto';
 
-const VideoProcessor = grpc.load(PROTO_PATH).VideoProcessor;
+// const VideoProcessor = grpc.load(PROTO_PATH).VideoProcessor;
 
-const grpc_client = new VideoProcessor('192.168.55.1:8080', grpc.credentials.createInsecure());
+// const grpc_client = new VideoProcessor('192.168.55.1:8080', grpc.credentials.createInsecure());
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {keepCase: true,
+   longs: String,
+   enums: String,
+   defaults: true,
+   oneofs: true
+  });
+var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+// The protoDescriptor object has the full package hierarchy
+console.log(protoDescriptor)
+var routeguide = protoDescriptor.VideoProcessor;
+console.log(routeguide)
+
+const grpc_client = new routeguide('192.168.55.1:8080', grpc.credentials.createInsecure());
+
 
 async function getImageUrl(client) {
   let response = null;
