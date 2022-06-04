2153de3b603eb80f89900419c4713d51fbe6f0f6
swh00tw <a6140000@gmail.com>
2022-06-04 17:44:14 +0800

    modify ip

--- src/service/gRPC_client.js
+++ src/service/gRPC_client.js
@@ -4,7 +4,7 @@ const PROTO_PATH = 'src/service/video.proto';
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true });
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 var routeguide = protoDescriptor.VideoProcessor;
-const grpc_client = new routeguide('172.20.10.4:8080', grpc.credentials.createInsecure());
+const grpc_client = new routeguide('101.12.54.161:8080', grpc.credentials.createInsecure());
 
 async function getImageUrl(client) {
   let response = null;
