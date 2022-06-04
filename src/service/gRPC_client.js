d4676dad14f4a792e297c691b50d6c2292784dca
swh00tw <a6140000@gmail.com>
2022-06-04 16:41:10 +0800

    modify ip

--- src/service/gRPC_client.js
+++ src/service/gRPC_client.js
@@ -1,27 +1,10 @@
 import * as grpc from 'grpc';
-// import protoLoader from "@grpc/proto-loader"
+const protoLoader = require('@grpc/proto-loader');
 const PROTO_PATH = __dirname + '/video.proto';
-
-// const VideoProcessor = grpc.load(PROTO_PATH).VideoProcessor;
-
-// const grpc_client = new VideoProcessor('192.168.55.1:8080', grpc.credentials.createInsecure());
-var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-  PROTO_PATH,
-  {keepCase: true,
-   longs: String,
-   enums: String,
-   defaults: true,
-   oneofs: true
-  });
-var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-// The protoDescriptor object has the full package hierarchy
-console.log(protoDescriptor)
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true });
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 var routeguide = protoDescriptor.VideoProcessor;
-console.log(routeguide)
-
-const grpc_client = new routeguide('192.168.55.1:8080', grpc.credentials.createInsecure());
-
+const grpc_client = new routeguide('172.20.10.4:8080', grpc.credentials.createInsecure());
 
 async function getImageUrl(client) {
   let response = null;
