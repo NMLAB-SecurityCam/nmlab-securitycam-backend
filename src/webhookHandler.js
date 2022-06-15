9851956ac9cab3113bf12e83a7e5df1720f66359
jimshao <jimmy25452@gmail.com>
2022-06-16 00:47:18 +0800

    add line_id to payload and whitelist on/off

--- src/webhookHandler.js
+++ src/webhookHandler.js
@@ -94,7 +94,7 @@ const webhookHandler = async (event, client) => {
     const userObj = await Users.findOne({ userId: event.source.userId });
     if (userObj?.userId) {
       // can do requets to ask the machine to take pics and save it in s3 and transfer it back here
-      publish(mqtt_publisher, mqtt_topic, { command: 'snapshot' });
+      publish(mqtt_publisher, mqtt_topic, { command: 'snapshot', line_id: event.source.userId });
       return client.replyMessage(event.replyToken, {
         type: 'text',
         text: 'Here is your snapshot.',
@@ -110,13 +110,13 @@ const webhookHandler = async (event, client) => {
   // enable or disable alert
   if (event.message.type === 'text' && event.message.text.slice(0, 7) === '!alert:') {
     if ((event.message.text.split(':')?.[1] ?? '').trim() === '1') {
-      publish(mqtt_publisher, mqtt_topic, { command: 'alert', enable: true });
+      publish(mqtt_publisher, mqtt_topic, { command: 'alert', enable: true , line_id: event.source.userId});
       return client.replyMessage(event.replyToken, {
         type: 'text',
         text: 'Enable alert feature.',
       });
     } else if ((event.message.text.split(':')?.[1] ?? '').trim() === '0') {
-      publish(mqtt_publisher, mqtt_topic, { command: 'alert', enable: false });
+      publish(mqtt_publisher, mqtt_topic, { command: 'alert', enable: false , line_id: event.source.userId});
       return client.replyMessage(event.replyToken, {
         type: 'text',
         text: 'Disable alert feature.',
@@ -129,7 +129,7 @@ const webhookHandler = async (event, client) => {
     if ((event.message.text.split(':')?.[1] ?? '').trim() === '1') {
       const userObj = await Users.findOne({ userId: event.source.userId });
       if (userObj?.streamingKey) {
-        publish(mqtt_publisher, mqtt_topic, { command: 'stream_on', stream_key: userObj.streamingKey });
+        publish(mqtt_publisher, mqtt_topic, { command: 'stream_on', stream_key: userObj.streamingKey , line_id: event.source.userId});
         return client.replyMessage(event.replyToken, {
           type: 'text',
           text: 'Start streaming.',
@@ -141,7 +141,7 @@ const webhookHandler = async (event, client) => {
         });
       }
     } else if ((event.message.text.split(':')?.[1] ?? '').trim() === '0') {
-      publish(mqtt_publisher, mqtt_topic, { command: 'stream_off' });
+      publish(mqtt_publisher, mqtt_topic, { command: 'stream_off' , line_id: event.source.userId});
       return client.replyMessage(event.replyToken, {
         type: 'text',
         text: 'Stop streaming.',
@@ -203,6 +203,46 @@ const webhookHandler = async (event, client) => {
     });
   }
 
+  // whitelist on/off
+  if (event.message.type === 'text' && event.message.text.slice(0, 11) === '!whitelist:') {
+    if ((event.message.text.split(':')?.[1] ?? '').trim() === '1') {
+      const userObj = await Users.findOne({ userId: event.source.userId });
+      if (userObj) {
+        publish(mqtt_publisher, mqtt_topic, { command: 'whitelist_on', line_id: event.source.userId});
+        return client.replyMessage(event.replyToken, {
+          type: 'text',
+          text: 'Whitelist ON.',
+        });
+      } else {
+        return client.replyMessage(event.replyToken, {
+          type: 'text',
+          text: 'Please log in first.',
+        });
+      }
+    } else if ((event.message.text.split(':')?.[1] ?? '').trim() === '0') {
+      publish(mqtt_publisher, mqtt_topic, { command: 'whitelist_off' , line_id: event.source.userId});
+      return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: 'Whitelist OFF.',
+      });
+    }
+  }
+  
+
+    // help message
+    if (event.message.type === 'text' && event.message.text.trim() === '!help') {
+      return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: '1. !id:{username} => Register\n'+
+        '2. !streaming_key:{streamingkey} => Set up youtube streaming\n'+
+        '3. !snapshot\n => Show current photo\n'+
+        '4. !alert:1/0 => Enable/Disable alert\n'+
+        '5. !stream:1/0 => Enable/Disable streaming\n'+
+        '6. !whitelist + send an image => Set whitelist\n'+
+        '7. !whitelist:1/0 => Enable/Disable Whitelist',
+      });
+    }
+
   /*
     implement response logic here,
     currently, just echo back the message
@@ -221,12 +261,13 @@ const webhookHandler = async (event, client) => {
   return client.replyMessage(event.replyToken, [
     {
       type: 'text',
-      text: event.message.text,
+      // text: event.message.text,
+      text: 'Invalid Commands. If you need instructions, type "!help"',
     },
-    {
-      type: 'text',
-      text: 'stfu',
-    },
+    // {
+    //   type: 'text',
+    //   text: 'stfu',
+    // },
   ]);
 };
 
